c487b0a8ed4e607fe7c4ae988b230fa5261a56ed
Boris Vinogradov <no111u3@gmail.com>
2023-11-09 16:58:38 +0100

    Change bindings to generated

--- build.rs
+++ build.rs
@@ -1,4 +1,18 @@
+use std::env;
+use std::path::PathBuf;
 fn main() {
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     println!("cargo:rustc-link-lib=framework=/Library/Frameworks/dwf.framework/dwf");
     println!("cargo:rustc-link-arg=-Wl,-rpath,/Library/Frameworks");
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg("-I/Library/Frameworks/dwf.framework/Headers")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
--- src/dwf.rs
+++ src/dwf.rs
@@ -1,14 +1,10 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-
-pub const __bool_true_false_are_defined: u32 = 1;
-pub const true_: u32 = 1;
-pub const false_: u32 = 0;
 pub type BOOL = ::std::os::raw::c_int;
 pub type BYTE = ::std::os::raw::c_uchar;
 
-include!("dwf_bind.rs");
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 pub fn to_c_bool(v: bool) -> BOOL {
     if v {
