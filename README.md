d3c6757cac07425724417fd87e8fe7441c4804e4
Boris Vinogradov <no111u3@gmail.com>
2023-11-09 17:25:10 +0100

    Add full select for platforms

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# dwf-rs
-Diligent Wave Form library Rust wrapper
+# Diligent Waveforms SDK library Rust wrapper
 
-tested only on Mac OS X
+[![docs](https://docs.rs/dwf/badge.svg)](https://docs.rs/dwf)
+[![crates.io](https://img.shields.io/crates/v/dwf.svg)](https://crates.io/crates/dwf)
 
 Non full implementation of the library API
+
+## Supported platforms
+
+Supported platforms: Mac OS X, Windows, Linux
+
+Tested: Mac OS X
+
+## License
+
+Licensed under
+
+* Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE))
--- build.rs
+++ build.rs
@@ -3,17 +3,24 @@ use std::path::PathBuf;
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    // TODO: implement this for other platforms
+    let include_path;
     if cfg!(target_os = "macos") {
         println!("cargo:rustc-link-lib=framework=/Library/Frameworks/dwf.framework/dwf");
         println!("cargo:rustc-link-arg=-Wl,-rpath,/Library/Frameworks");
+        include_path = "/Library/Frameworks/dwf.framework/Headers";
+    } else if cfg!(target_os = "windows") {
+        println!("cargo:rustc-link-lib=dylib=dwf.dll");
+        include_path = "C:\\Program Files (x86)\\Digilent\\WaveFormsSDK\\inc"
+    } else if cfg!(target_os = "linux") {
+        println!("cargo:rustc-link-lib=dylib=libdwf.so");
+        include_path = "/usr/include/digilent/waveforms"
     } else {
-        unimplemented!("Mac OS is supported");
+        unimplemented!("Only Linux, Mac OS and Windows are supported");
     }
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-I/Library/Frameworks/dwf.framework/Headers")
+        .clang_arg(format!("-I{}",include_path))
         .generate()
         .expect("Unable to generate bindings");
 
